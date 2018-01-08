73a85bcb3ce46e6bfd3747076f3e2400562284ed
Julien Le Hericy <jlh@Juliens-iMac.local>
2018-01-08 09:43:29 +0100

    Issue #1 - Add Automation (Docker, K8s and Jenkins)

---
+++ Jenkinsfile
@@ -0,0 +1,71 @@
+pipeline {
+  environment {
+    project_name = "cessda-development"
+    app_name = "pasc-osmh"
+    feSvc_name = "${app_name}-service"
+    namespace = "cessda-pasc"
+    image_tag = "eu.gcr.io/${project_name}/${app_name}:v${env.BUILD_NUMBER}"
+  }
+
+  agent any
+
+  stages {
+    stage('Check environment') {
+      steps {
+	      echo "Check environment"
+        echo "project_name = ${project_name}"
+        echo "app_name = ${app_name}"
+        echo "feSvc_name = ${feSvc_name}"
+        echo "namespace = ${namespace}"
+        echo "JOB_NAME = ${JOB_NAME}"
+        echo "image_tag = ${image_tag}"
+      }
+    }
+    stage('Build Project and start Sonar scan') {
+		  steps {
+        withSonarQubeEnv('cessda-sonar') {
+          sh 'mvn clean install sonar:sonar -Dsonar.projectName=$JOB_NAME -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}'
+          sleep 5
+        }
+      }
+    }
+    stage('Get Quality Gate Status') {
+      steps {
+        withSonarQubeEnv('cessda-sonar') {
+          sh 'curl -su ${SONAR_AUTH_TOKEN}: ${SONAR_HOST_URL}api/qualitygates/project_status?analysisId="$(curl -su ${SONAR_AUTH_TOKEN}: ${SONAR_HOST_URL}api/ce/task?id="$(cat target/sonar/report-task.txt | awk -F "=" \'/ceTaskId=/{print $2}\')" | jq -r \'.task.analysisId\')" | jq -r \'.projectStatus.status\' > status'
+        }
+        script {
+          STATUS = readFile('status')
+          if ( STATUS.trim() == "ERROR") {
+            error("Quality Gate not reached, please review the Sonar Report")
+          } else if ( STATUS.trim() == "WARN") {
+            error("Quality Gate not reached, please review the Sonar Report")
+          } else {
+            echo "Quality Gate reached, deployment will be processed, please wait"
+          }
+        }
+      }
+    }
+	  stage('Build Docker image') {
+   		steps {
+		  echo "Build Docker image"
+                  sh("gcloud docker -- pull eu.gcr.io/cessda-development/cessda-java:8")
+                  sh("docker build -t ${image_tag} .")
+      }
+    }
+    stage('Push Docker image') {
+      steps {
+		    echo "Push Docker image"
+        sh("gcloud docker -- push ${image_tag}")
+        sh("gcloud container images add-tag ${image_tag} eu.gcr.io/${project_name}/${app_name}:latest")
+      }
+    }
+    stage('Check Requirements and Deployments') {
+      steps {
+        dir('./infrastructure/gcp/') {
+          sh("bash pasc-osmh-creation.sh")
+        }
+      }
+    }
+  }
+}
---
+++ infrastructure/gcp/pasc-osmh-creation.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#############################
+#          CESSDA           #
+#      Cluster Setup        #
+#############################
+
+# Julien Le Hericy
+# CESSDA ERIC
+# j.lehericy(at)cessda.eu
+
+
+### Load Env Variable and set gcloud Region/Zone/Project
+source ./gcp.config > /dev/null 2>&1
+
+### Kubernetes configuration generation ###
+sed "s/DEPLOYMENTNAME/$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$PRODUCT-$ENVIRONMENT/g; s/PVCNAME/$PRODUCT-$ENVIRONMENT-pvc/g" ../k8s/template-pasc-$MODULE-deployment.yaml > ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
+sed "s/SERVICENAME/$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$PRODUCT-$ENVIRONMENT/g" ../k8s/template-pasc-$MODULE-service.yaml > ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
+
+# Kubctl credentials setup
+gcloud container clusters get-credentials $PRODUCT-$ENVIRONMENT-cc --zone=$ZONE > /dev/null 2>&1
+
+# Deployment
+if kubectl get deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+  then
+    echo "Deployment already exists, it will be destroyed to perform the new deployment"
+    kubectl delete deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1
+    kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
+    echo "Deployment created"
+  else
+    kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
+    echo "Deployment created"
+fi;
+
+# Service
+if kubectl get service $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+  then
+    echo "Service already exists"
+  else
+    kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml > /dev/null 2>&1
+    echo "Service created"
+fi;
+
+### Clean Workspace
+rm -rf ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
+rm -rf ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
+
+
+# Wait 30s
+sleep 30s
+
+### DNS Records
+kubectl get pods -n $PRODUCT-$ENVIRONMENT -o wide
+kubectl get pods -n $PRODUCT-$ENVIRONMENT -o wide | grep "$PRODUCT-$ENVIRONMENT" | awk '{print $6,$1}' >> ./hosts
+kubectl exec $(kubectl get pods -n $PRODUCT-$ENVIRONMENT | grep "admin" | awk '{print $1}') -n $PRODUCT-$ENVIRONMENT -- /bin/sh -c "touch /tmp/hosts"
+kubectl cp ./hosts $(kubectl get pods -n $PRODUCT-$ENVIRONMENT | grep "admin" | awk '{print $1}'):/tmp/hosts -n $PRODUCT-$ENVIRONMENT
+kubectl exec $(kubectl get pods -n $PRODUCT-$ENVIRONMENT | grep "admin" | awk '{print $1}') -n $PRODUCT-$ENVIRONMENT -- /bin/sh -c "cat /tmp/hosts > /etc/hosts"
