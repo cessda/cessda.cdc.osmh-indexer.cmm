4d013650c27239e0d74dd75900fabd12220cde83
Julien Le Hericy <j.lehericy@cessda.eu>
2018-01-31 15:12:43 +0100

    - #1 Automation, Kubernetes and Docker basic configuration

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# OpenJDK Base Container
+FROM eu.gcr.io/cessda-development/cessda-java:latest
+
+# Container Information
+MAINTAINER CESSDA-ERIC "support@cessda.eu"
+
+# Create Volume tmp and add JAR artifacts
+VOLUME /tmp
+ADD ./target/pasc-osmh-indexer*.jar pasc-osmh-indexer.jar
+
+# Java options
+ENV JAVA_OPTS ""
+
+# Entrypoint - Start Admin
+ENTRYPOINT exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /pasc-osmh-indexer.jar -Dspring.profiles.active=dev
---
+++ infrastructure/gcp/pasc-osmh-registration.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#############################
+#          CESSDA           #
+#      Cluster Setup        #
+#############################
+
+# Julien Le Hericy
+# CESSDA ERIC
+# j.lehericy(at)cessda.eu
+
+
+### Load Env Variable and set gcloud Region/Zone/Project
+source ./gcp.config > /dev/null 2>&1
+
+# Kubctl credentials setup
+gcloud container clusters get-credentials $PRODUCT-$ENVIRONMENT-cc --zone=$ZONE > /dev/null 2>&1
+
+### Kubernetes Deployment ###
+if kubectl get deployment $PRODUCT-admin-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+  then
+    echo "Admin component available, deployment will be processed"
+    echo "Setup for Spring Boot Admin Registration"
+    sed -i "s/APPNAME/$PRODUCT-$MODULE-$ENVIRONMENT/g; s/localhost:8087/$PRODUCT-admin-$ENVIRONMENT:8087/g" ../../src/main/resources/application.yml
+  else
+    echo "Admin component not available, deployment's aborted"
+    exit 1
+fi;
