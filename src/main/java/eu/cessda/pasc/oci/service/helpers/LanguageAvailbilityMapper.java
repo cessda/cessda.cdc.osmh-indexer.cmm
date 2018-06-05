08c19f5b4a3ed8c281c6d2ef0dc16f465398f155
Moses Mansaray <moses@doraventures.com>
2018-06-05 21:22:14 +0100

    #58 - Provide content for list of languages for which other metadata is available - WIP

---
+++ src/main/java/eu/cessda/pasc/oci/service/helpers/LanguageAvailbilityMapper.java
@@ -0,0 +1,13 @@
+package eu.cessda.pasc.oci.service.helpers;
+
+import eu.cessda.pasc.oci.models.cmmstudy.CMMStudy;
+
+/**
+ * @author moses@doraventures.com
+ */
+public class LanguageAvailbilityMapper {
+
+  static CMMStudy applyAvaiableLangs(CMMStudy cMMStudiesOptions){
+    throw new RuntimeException("Not yet impl");
+  }
+}
