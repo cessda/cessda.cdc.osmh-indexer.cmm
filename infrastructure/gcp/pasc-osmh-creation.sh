aff11b07d7fb6a9d05ddd036d7134cc887767f38
John Shepherdson <john.shepherdson@cessda.eu>
2018-12-11 08:30:27 +0000

    Removed output redirection, so can see it in console and logs

--- infrastructure/gcp/pasc-osmh-creation.sh
+++ infrastructure/gcp/pasc-osmh-creation.sh
@@ -18,13 +18,13 @@ sed "s/DEPLOYMENTNAME/$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$PRODUCT-$ENV
 sed "s/SERVICENAME/$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$PRODUCT-$ENVIRONMENT/g" ../k8s/template-pasc-$MODULE-service.yaml > ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
 
 # Kubctl credentials setup
-gcloud container clusters get-credentials $PRODUCT-$ENVIRONMENT-cc --zone=$ZONE > /dev/null 2>&1
+gcloud container clusters get-credentials $PRODUCT-$ENVIRONMENT-cc --zone=$ZONE ### > /dev/null 2>&1
 
 # Deployment
-if kubectl get deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+if kubectl get deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT; ### > /dev/null 2>&1;
   then
     echo "Deployment already exists, it will be destroyed to perform the new deployment"
-    kubectl delete deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1
+    kubectl delete deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT ### > /dev/null 2>&1
     kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
     echo "Deployment created"
   else
@@ -33,10 +33,10 @@ if kubectl get deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT
 fi;
 
 # Service
-if kubectl get service $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+if kubectl get service $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT; ### > /dev/null 2>&1;
   then
     echo "Service already exists"
   else
-    kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml > /dev/null 2>&1
+    kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml ### > /dev/null 2>&1
     echo "Service created"
 fi;
--- infrastructure/gcp/pasc-osmh-registration.sh
+++ infrastructure/gcp/pasc-osmh-registration.sh
@@ -14,15 +14,15 @@
 source ./gcp.config > /dev/null 2>&1
 
 # Kubctl credentials setup
-gcloud container clusters get-credentials $PRODUCT-$ENVIRONMENT-cc --zone=$ZONE > /dev/null 2>&1
+gcloud container clusters get-credentials $PRODUCT-$ENVIRONMENT-cc --zone=$ZONE ###> /dev/null 2>&1
 
 ### Kubernetes Deployment ###
-if kubectl get deployment $PRODUCT-admin-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+if kubectl get deployment $PRODUCT-admin-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT; ### > /dev/null 2>&1;
   then
     echo "Admin component available, deployment will be processed"
     echo "Setup for Spring Boot Admin Registration"
     sed -i "s/APPNAME/$PRODUCT-$MODULE-$ENVIRONMENT/g; s/localhost:8087/$PRODUCT-admin-$ENVIRONMENT:8087/g" ../../src/main/resources/application.yml
   else
-    echo "Admin component not available, deployment's aborted"
+    echo "Admin component not available, deployment aborted"
     exit 1
 fi;
