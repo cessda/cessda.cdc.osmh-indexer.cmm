bcd3bfc87c99460e78360b7294d3f0deff9f439d
John Shepherdson <john.shepherdson@cessda.eu>
2019-04-24 15:08:19 +0100

    Added DNA endpoint

--- src/test/java/eu/cessda/pasc/osmhhandler/oaipmh/configuration/HandlerConfigurationPropertiesTest.java
+++ src/test/java/eu/cessda/pasc/osmhhandler/oaipmh/configuration/HandlerConfigurationPropertiesTest.java
@@ -42,15 +42,17 @@ public class HandlerConfigurationPropertiesTest {
     then(oaiPmh.getRepos().get(1).getPreferredMetadataParam()).isEqualTo("oai_ddi25_nl");
     then(oaiPmh.getRepos().get(2).getUrl()).isEqualTo("https://easy.dans.knaw.nl/oai/");
     then(oaiPmh.getRepos().get(2).getPreferredMetadataParam()).isEqualTo("oai_ddi25_en");
-    then(oaiPmh.getRepos().get(3).getUrl()).isEqualTo("http://services.fsd.uta.fi/v0/oai");
+    then(oaiPmh.getRepos().get(3).getUrl()).isEqualTo("https://kuha.sa.dk/v0/oai");
     then(oaiPmh.getRepos().get(3).getPreferredMetadataParam()).isEqualTo("oai_ddi25");
-    then(oaiPmh.getRepos().get(4).getUrl()).isEqualTo("https://dbk.gesis.org/dbkoai3/");
+    then(oaiPmh.getRepos().get(4).getUrl()).isEqualTo("http://services.fsd.uta.fi/v0/oai");
     then(oaiPmh.getRepos().get(4).getPreferredMetadataParam()).isEqualTo("oai_ddi25");
-    then(oaiPmh.getRepos().get(5).getUrl()).isEqualTo("http://147.213.4.44:6003/v0/oai");
+    then(oaiPmh.getRepos().get(5).getUrl()).isEqualTo("https://dbk.gesis.org/dbkoai3/");
     then(oaiPmh.getRepos().get(5).getPreferredMetadataParam()).isEqualTo("oai_ddi25");
-    then(oaiPmh.getRepos().get(6).getUrl()).isEqualTo("https://snd.gu.se/en/oai-pmh");
+    then(oaiPmh.getRepos().get(6).getUrl()).isEqualTo("http://147.213.4.44:6003/v0/oai");
     then(oaiPmh.getRepos().get(6).getPreferredMetadataParam()).isEqualTo("oai_ddi25");
-    then(oaiPmh.getRepos().get(7).getUrl()).isEqualTo("https://oai.ukdataservice.ac.uk:8443/oai/provider");
+    then(oaiPmh.getRepos().get(7).getUrl()).isEqualTo("https://snd.gu.se/en/oai-pmh");
+    then(oaiPmh.getRepos().get(7).getPreferredMetadataParam()).isEqualTo("oai_ddi25");
+    then(oaiPmh.getRepos().get(8).getUrl()).isEqualTo("https://oai.ukdataservice.ac.uk:8443/oai/provider");
     then(oaiPmh.getRepos().get(7).getPreferredMetadataParam()).isEqualTo("ddi");
   }
 }
