f47f5af5460fd7e5ab0698521b75955227d3a584
Matthew Morris <matthew.morris@cessda.eu>
2020-06-19 15:10:11 +0200

    #188: Expose Prometheus endpoint

--- src/main/java/eu/cessda/pasc/oci/configurations/SecurityConfiguration.java
+++ src/main/java/eu/cessda/pasc/oci/configurations/SecurityConfiguration.java
@@ -17,6 +17,7 @@ package eu.cessda.pasc.oci.configurations;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,6 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
+                .requestMatchers(EndpointRequest.to(PrometheusScrapeEndpoint.class)).permitAll()
                 .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR") // Secure actuator endpoints
                 .anyRequest().permitAll() // Enable access to REST APIs without authentication
                 .and().formLogin() // Enable form based login
