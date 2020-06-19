b8e464d929a52cd4f576ed7b17042c1212cc2068
Matthew Morris <matthew.morris@cessda.eu>
2020-06-19 13:25:15 +0200

    #188: Allow Kubernetes access to the health endpoint

--- src/main/java/eu/cessda/pasc/oci/configurations/SecurityConfiguration.java
+++ src/main/java/eu/cessda/pasc/oci/configurations/SecurityConfiguration.java
@@ -16,6 +16,7 @@
 package eu.cessda.pasc.oci.configurations;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,7 +30,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
      */
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR") // Secure actuator endpoints
+        http.authorizeRequests().requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR") // Secure actuator endpoints
                 .anyRequest().permitAll() // Enable access to REST APIs without authentication
                 .and().formLogin() // Enable form based login
                 .and().httpBasic() // Enable basic authentication
