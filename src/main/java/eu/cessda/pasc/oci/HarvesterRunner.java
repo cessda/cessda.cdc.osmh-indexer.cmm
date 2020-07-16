a15c828e76e5ae9cf2bebc730224b91c05394088
John Shepherdson <john.shepherdson@cessda.eu>
2020-07-16 17:01:42 +0100

    Fix static analysis warnings. #203

--- src/main/java/eu/cessda/pasc/oci/HarvesterRunner.java
+++ src/main/java/eu/cessda/pasc/oci/HarvesterRunner.java
@@ -53,6 +53,7 @@ public class HarvesterRunner {
     private final LanguageAvailabilityMapper languageAvailabilityMapper;
     private final Metrics metrics;
     private final HarvesterConsumerService remoteHarvester;
+    private String result;
 
     private final AtomicBoolean indexerRunning = new AtomicBoolean(false);
 
@@ -91,6 +92,7 @@ public class HarvesterRunner {
                         langStudies.forEach((langIsoCode, cmmStudies) -> {
                             try (var langClosable = MDC.putCloseable(LoggingConstants.LANG_CODE, langIsoCode)) {
                                 executeBulk(repo, langIsoCode, cmmStudies);
+                                result = langClosable.toString();
                             }
                         });
                     }
